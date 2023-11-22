dfaa77421475d4f3910990c5b29e9d9a212e9043
KOGASA0X <106465740+KOGASA0X@users.noreply.github.com>
2023-11-22 12:13:13 +0000

    feat: create Q1.R and Q2.R

---
+++ Q1/Q1.R
@@ -0,0 +1,81 @@
+#Q1
+setwd("C:/Users/stcfr/Desktop/Assignment01_Data")
+#read data from file
+CCS592 <- readLines("Q1_CCS592.txt")
+CDS501 <- readLines("Q1_CDS501.txt")
+CDS506 <- readLines("Q1_CDS506.txt")
+CDS512 <- readLines("Q1_CDS512.txt")
+CDS521 <- readLines("Q1_CDS521.txt")
+CDS523 <- readLines("Q1_CDS523.txt")
+
+#a
+num_student <- c( CCS592 = length(CCS592), CDS501 = length(CDS501), CDS506 = length(CDS506), CDS512 = length(CDS512), CDS521 = length(CDS521), CDS523 = length(CDS523) )
+#Calculate highest number of the student
+max_course <- names(num_student)[which.max(num_student)]
+max_student <- max(num_student)
+
+cat("Course with the highest number of students:", max_course, "\n")
+cat("Number of students:", max_student, "\n")
+
+#Calculate lowest number of the student
+min_course <- names(num_student)[which.min(num_student)]
+min_student <- min(num_student)
+
+cat("Course with the lowest number of students:", min_course, "\n")
+cat("Number of students:", min_student, "\n")
+
+#b
+#Combine student of six courses
+total_student_course <- c(CCS592, CDS501, CDS506, CDS512, CDS521, CDS523)
+
+# Calculate distinct students
+distinct_student <- unique(total_student_course)
+student_count <- length(distinct_student)
+cat("Number of distinct students across all six courses:", student_count, "\n")
+
+#c
+course_registed_student <- function(student_name){
+  
+  course <- c("CCS592", "CDS501", "CDS506", "CDS512", "CDS521", "CDS523")
+  student_name_registered_course <- list(CCS592, CDS501, CDS506, CDS512, CDS521, CDS523)
+  student_course_link <- setNames(student_name_registered_course, course)
+  
+  # initial student_course
+  student_courses <- character(0)
+  #check all the course that registered by student in each course
+  for (course in course) {
+    if (student_name %in% student_course_link[[course]]) {
+      student_courses <- c(student_courses, course)
+    }
+  }
+  #print the student name with course registered
+  cat("Courses registered by", student_name, ":", paste(student_courses, collapse = ", "), "\n")
+  
+}
+course_registed_student("NAME003")
+#d
+student_CDS512_CDS521 <- intersect(CDS512,CDS521)
+cat("Students registered for both CDS512 and CDS521:", paste(student_CDS512_CDS521, collapse = ", "), "\n")
+#e
+student_CDS512_NO_CDS521 <- setdiff(CDS512,CDS521)
+cat("Students registered for CDS512 but not CDS521:", paste(student_CDS512_NO_CDS521, collapse = ", "), "\n")
+#f
+student_CDS521_NO_CDS512 <- setdiff(CDS521,CDS512)
+cat("Students registered for CDS521 but not CDS512:", paste(student_CDS521_NO_CDS512, collapse = ", "), "\n")
+#g
+CDS512_CDS521 <- union(CDS512, CDS521)
+all_course_without_CDS512_CDS521 <- union(union(CCS592, CDS501), union(CDS506, CDS523))
+student_NO_CDS521_NO_CDS512 <- setdiff(all_course_without_CDS512_CDS521, CDS512_CDS521)
+cat("Students who did not register for CDS512 nor CDS521:", paste(student_NO_CDS521_NO_CDS512, collapse = ", "), "\n")
+#h
+# Create a data frame to store the counts of courses for each student
+course_counts <- data.frame(Student = distinct_student, Count = 0)
+
+# Use a for loop to count the number of courses for each student
+for (i in seq_along(course_counts$Student)) {
+  student <- course_counts$Student[i]
+  count <- sum(student %in% CDS501, student %in% CDS506, student %in% CDS512, student %in% CDS521, student %in% CDS523, student %in% CCS592)
+  course_counts$Count[i] <- count
+}
+students_in_three_courses <- course_counts$Student[course_counts$Count == 3]
+cat("Students who registered for three courses:", paste(students_in_three_courses, collapse = ", "), "\n")
---
+++ Q2/Q2.R
@@ -0,0 +1,36 @@
+#Q2
+# Initialize variables to store word counts
+analytics_num <- 0
+insight_num <- 0
+of_num <- 0
+
+# Function to count words in a given text from file
+count_words <- function(text) {
+  # Convert text to lowercase for case-insensitive matching
+  text <- tolower(text)
+  
+  # Tokenise the text into words
+  words <- unlist(strsplit(text, "\\W+"))
+  
+  # Count occurrences of specified words
+  analytics_num <<- analytics_num + sum(words == "analytics")
+  insight_num <<- insight_num + sum(words == "insight")
+  of_num <<- of_num + sum(words == "of")
+}
+
+# Loop through all the text file
+for (i in 1:10) {
+  # Read the content of the file
+  file_path <- paste("Q2_Part_", sprintf("%02d", i), ".txt", sep="")
+  file_content <- tolower(readLines(file_path, warn = FALSE))
+  
+  # Combine lines into a single string
+  file_text <- paste(file_content, collapse = " ")
+  
+  # Count words in the text
+  count_words(file_text)
+}
+#Print the word count of "analytics", "insight", "of" 
+cat("Total number count of 'analytics':", analytics_num, "\n")
+cat("Total number count of 'insight':", insight_num, "\n")
+cat("Total number count of 'of':", of_num, "\n")
