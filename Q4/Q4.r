ad1faa7385778a03b296c05f1577cbbb7d5252c2
KOGASA0X <106465740+KOGASA0X@users.noreply.github.com>
2023-11-22 12:32:23 +0000

    update output.png and Q4.r

--- Q4/Q4.r
+++ Q4/Q4.r
@@ -1,22 +1,22 @@
-# 读取CSV文件
+# Read CSV file
 data <- read.csv("tracks_features.csv")
 
-# 计算文件数量
+# Calculate the number of files
 num_files <- 40
 
-# 对于每个文件
+# For each file
 for (i in 1:num_files) {
-  # 计算列的范围
-  start_col <- ((i - 1) %% (ncol(data) / 3)) * 3 + 1
-  end_col <- min(start_col + 2, ncol(data))
-  
-  # 计算行的范围
-  start_row <- ((i - 1) %/% (ncol(data) / 3)) * 250000 + 1
-  end_row <- min(start_row + 249999, nrow(data))
-  
-  # 选择相应的行和列
-  subset <- data[start_row:end_row, start_col:end_col]
-  
-  # 写入新的CSV文件
-  write.csv(subset, paste0("Q4/spotify_", i, ".csv"), row.names = FALSE)
+    # Calculate the range of columns
+    start_col <- ((i - 1) %% (ncol(data) / 3)) * 3 + 1
+    end_col <- min(start_col + 2, ncol(data))
+    
+    # Calculate the range of rows
+    start_row <- ((i - 1) %/% (ncol(data) / 3)) * 250000 + 1
+    end_row <- min(start_row + 249999, nrow(data))
+    
+    # Select the corresponding rows and columns
+    subset <- data[start_row:end_row, start_col:end_col]
+    
+    # Write to a new CSV file
+    write.csv(subset, paste0("Q4/spotify_", i, ".csv"), row.names = FALSE)
 }
