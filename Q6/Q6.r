a375a2d8ddcbd49e2bf5a69c6f7b71e7b0651836
DebrisKaiser <81285017+DebrisKaiser@users.noreply.github.com>
2023-11-26 13:13:47 +0000

    Refactor Sudoku feasibility check function

--- Q6/Q6.r
+++ Q6/Q6.r
@@ -3,12 +3,12 @@ check_row <- function(row) {
   n <- length(row)
   for (i in 1:(n-1)) {
     for (j in (i+1):n) {
-      if (row[i] == row[j] && row[i] != "C")  {
-        return(FALSE)
+      if (row[i] == row[j] && row[i] != "C") {
+        return(FALSE)  # Returns FALSE if a duplicate (excluding "C") is found in the row
       }
     }
   }
-  return(TRUE)
+  return(TRUE)  # Returns TRUE if no duplicates are found in the row
 }
 
 # Define a function to check whether there are duplicate numbers or letters in a column
@@ -17,11 +17,11 @@ check_column <- function(column) {
   for (i in 1:(n-1)) {
     for (j in (i+1):n) {
       if (column[i] == column[j] && column[i] != "C") {
-        return(FALSE)
+        return(FALSE)  # Returns FALSE if a duplicate (excluding "C") is found in the column
       }
     }
   }
-  return(TRUE)
+  return(TRUE)  # Returns TRUE if no duplicates are found in the column
 }
 
 # Define a function to check if there are duplicate numbers or letters in a 3x4 block
@@ -30,66 +30,63 @@ check_block <- function(block) {
   for (i in 1:(n-1)) {
     for (j in (i+1):n) {
       if (block[i] == block[j] && block[i] != "C") {
-        return(FALSE)
+        return(FALSE)  # Returns FALSE if a duplicate (excluding "C") is found in the block
       }
     }
   }
-  return(TRUE)
+  return(TRUE)  # Returns TRUE if no duplicates are found in the block
 }
 
-# Read the input file 1, map numbers and letters to the matrix
-input1 <- read.table("Q6/Q6_Input_01.txt", header = FALSE, sep = ";")
-matrix1 <- matrix(nrow = 12, ncol = 12)
-for (i in 1:nrow(input1)) {
-  for (j in 1:ncol(input1)) {
-    matrix1[i, j] <- input1[i, j]
+# Function to read the input files and check Sudoku feasibility
+check_sudoku <- function(file_path) {
+  input_data <- read.table(file_path, header = FALSE, sep = ";")
+  sudoku_matrix <- matrix(nrow = 12, ncol = 12)
+  
+  # Map values to the 12x12 matrix
+  for (i in 1:nrow(input_data)) {
+    for (j in 1:ncol(input_data)) {
+      sudoku_matrix[i, j] <- input_data[i, j]
+    }
   }
-}
-
-# Read the input file 2 and check whether Sudoku is feasible
-input2 <- read.table("Q6/Q6_Input_02.txt", header = FALSE, sep = ";")  
-for (i in 1:nrow(input2)) {  
-  row <- input2[i, ]  
-  if (!check_row(matrix1[i, ])) {  
-    cat("Row violation:", i, "\n")  
-    return(FALSE)  
-  }  
-  if (!check_column(matrix1[, i])) {  
-    cat("Column violation:", i, "\n")  
-    return(FALSE)  
-  }  
-  if (!check_block(matrix1[(i-3)*4+1:(i-3)*4+4, ])) {  
-    cat("Block violation:", i, "\n")  
-    return(FALSE)  
-  }  
-}  
-
-# Check rows, columns, and blocks of a matrix
-check_matrix <- function(matrix) {
-  for (i in 1:nrow(matrix)) {
-    if (!check_row(matrix[i, ])) {
+  
+  # Check rows, columns, and blocks
+  for (i in 1:nrow(sudoku_matrix)) {
+    if (!check_row(sudoku_matrix[i, ])) {
+      cat("Row violation in row:", i, "\n")
       return(FALSE)
     }
   }
-  for (j in 1:ncol(matrix)) {
-    if (!check_column(matrix[, j])) {
+  
+  for (j in 1:ncol(sudoku_matrix)) {
+    if (!check_column(sudoku_matrix[, j])) {
+      cat("Column violation in column:", j, "\n")
       return(FALSE)
     }
   }
-  for (i in seq(1, nrow(matrix), by=3)) {
-    for (j in seq(1, ncol(matrix), by=4)) {
-      block <- c(matrix[i:(i+2), j:(j+3)])
+  
+  for (i in seq(1, nrow(sudoku_matrix), by = 3)) {
+    for (j in seq(1, ncol(sudoku_matrix), by = 4)) {
+      block <- c(sudoku_matrix[i:(i+2), j:(j+3)])
       if (!check_block(block)) {
+        cat("Block violation in block starting at row:", i, "and column:", j, "\n")
         return(FALSE)
       }
     }
   }
+  
   return(TRUE)
 }
 
-# Call the function to check the matrix
-if (check_matrix(matrix1)) {
-  cat("Matrix 1 is valid.")
+# Call the function to check the feasibility of Input File 1
+if (check_sudoku("Q6/Q6_Input_01.txt")) {
+  cat("Input File 1 is a feasible puzzle.")
 } else {
-  cat("Matrix 1 is invalid.")
+  cat("Input File 1 is an infeasible puzzle.")
+}
+
+# Call the function to check the feasibility of Input File 2
+if (check_sudoku("Q6/Q6_Input_02.txt")) {
+  cat("Input File 2 is a feasible puzzle.")
+} else {
+  cat("Input File 2 is an infeasible puzzle.")
 }
